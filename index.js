c11e9ce869bcca6e0cfa5c4df6d5a88e23c423f2
KGH1113 <123816263+KGH1113@users.noreply.github.com>
2023-08-31 21:13:33 +0900

    port bug fixed

--- index.js
+++ index.js
@@ -466,7 +466,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-const port = 2000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
